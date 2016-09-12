cd67ff6553feb2bbed8380ec08ccc5cfb67c3820
Brice Tessier Huort <tessierhuort@gmail.com>
2016-09-12 16:41:49 +0200

    problem with activites#index dont manage the col

--- app/controllers/proposals_controller.rb
+++ app/controllers/proposals_controller.rb
@@ -1,7 +1,37 @@
 class ProposalsController < ApplicationController
+  before_action :set_activity
 
   def index
     @proposals = Proposal.all
-    @activity = Activity.find(params[:id])
+    @proposal = Proposal.new
+  end
+
+  def new
+    @proposal = Proposal.new
+  end
+
+
+  def create
+    @proposal = Proposal.new(proposal_params)
+    @proposal.activity = @activity #une proposal belongs to  activity_id
+      if @proposal.save
+        redirect_to activity_proposals_path
+      else
+        render :new
+    end
+  end
+
+  def destroy
+    @proposal = Proposal.find(params[:id])
+  end
+
+  private
+
+  def set_activity
+    @activity = Activity.find(params[:activity_id]) #on a besoin de choper l'activity_id car proposal est nesté dans activity
+  end
+
+  def proposal_params
+    params.require(:proposal).permit(:subject, :content, :location, :price, :activity_id)
   end
 end
